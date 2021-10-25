3374fa4d13755fc13391196aaff83649916207af
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2021-10-25 23:50:38 +0300

    Initial Commit

---
+++ CodeUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeUI
+//
+//  Created by Pavel Paddubotski on 25.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
